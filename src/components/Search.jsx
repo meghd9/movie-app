1275228a7595ce9c683510c0512f5a495a617bfd
Megh <meghd9@gmail.com>
2025-09-03 19:07:01 +0100

    Second commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 const Search = ({ searchTerm, setSearchTerm }) => {
   console.log("working)")
+  console.log("workingggg 2)")
+
   return (
     <div className="search">
       <div>
